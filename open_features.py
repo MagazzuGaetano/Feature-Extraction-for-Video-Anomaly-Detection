687db007abb1fa7ce8963e0d05592ef8c190526e
lfx <lordfighterx@gmail.com>
2023-02-27 14:14:57 +0100

    update files to check

--- open_features.py
+++ open_features.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-file1 = np.load('/home/ubuntu/Desktop/Activity-Recognition-Feature-Extraction-for-W-VAD/output/Explosion036_x264.npy')
+file1 = np.load('/home/ubuntu/Desktop/Activity-Recognition-Feature-Extraction-for-W-VAD/output/01_002.npy')
 file1 = file1.astype('float32')
 print(np.asarray(file1).shape)
 
 
-file2 = np.load('/home/ubuntu/Desktop/RTFM/features/UCF_Test_ten_crop_i3d(corrupted)/Explosion036_x264_i3d.npy')
+file2 = np.load('/home/ubuntu/Desktop/RTFM/features/ShanghaiTech_features/SH_Train_ten_crop_i3d/01_002_i3d.npy')
 file2 = file2.astype('float32')
 print(np.asarray(file2).shape)
 
