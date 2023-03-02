f0815b46a73fc5f065f50d60089d566a497c66d7
lfx <lordfighterx@gmail.com>
2023-03-02 12:29:24 +0100

    code optimization

--- main.py
+++ main.py
@@ -1,103 +1,120 @@
 import os
 from pathlib import Path
-import shutil
 import argparse
 from natsort import natsorted
 import numpy as np
-import time
 import ffmpeg
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from torch.autograd import Variable
-import torchvision
 from models.resnet import i3_res50
 from utils.utils import from_frames_to_clips, load_rgb_batch, split_clip_indices_into_batches
 
 
-
-def extract_features(i3d, clip_size, frequency, temppath, batch_size):
-
+def extract_features(i3d, device, clip_length, temp_path, batch_size):
     # rgb files
-    rgb_files = natsorted([i for i in os.listdir(temppath)])
+    rgb_files = natsorted([i for i in os.listdir(temp_path)])
 
     # creating clips
-    frame_indices = from_frames_to_clips(rgb_files)
+    frame_indices = from_frames_to_clips(rgb_files, clip_length)
 
     # divide clips into batches (for memory optimization)
-    # a batch of dimension 16 it's equal to a 16-frames clip
-    frame_indices = split_clip_indices_into_batches(frame_indices, batch_size)
+    # k = true values in the last batch
+    frame_indices, k = split_clip_indices_into_batches(frame_indices, batch_size)
+
+    n_batches = frame_indices.shape[0]
+    n_clips_inside_a_batch = frame_indices.shape[1]
 
     # iterate 10-crop augmentation
-    full_features = [[] for i in range(10)]
-    number_of_batches = frame_indices.shape[0]
-
-    for batch_id in range(number_of_batches):
+    full_features = torch.zeros((n_batches, 10, n_clips_inside_a_batch, 2048, 1, 1, 1), device=device)
+    for batch_id in range(n_batches):
 
         # B=16 x T=16 x CROP=10 x CH=3 x H=224 x W=224
-        batch_data = load_rgb_batch(temppath, rgb_files, frame_indices[batch_id]) 
+        batch_data = load_rgb_batch(temp_path, rgb_files, frame_indices[batch_id], device)
 
         # iterate 10-crop augmentation
-        for i in range(batch_data.shape[2]):
-            b_data = batch_data[:,:,i,:,:,:] # select the i-crop
-            b_data = torch.from_numpy(b_data)
-            b_data = b_data.transpose(1, 2) # B=16 x CH=3 x T=16 x H=224 x W=224
+        n_crops = batch_data.shape[2]
+        for i in range(n_crops):
+            b_data = batch_data[:, :, i, :, :, :]  # select the i-crop
+            b_data = b_data.transpose(1, 2)  # B=16 x CH=3 x T=16 x H=224 x W=224
 
             with torch.no_grad():
-                b_data = Variable(b_data.cuda()).float()
                 inp = {'frames': b_data}
                 features = i3d(inp)
 
-            v = features.cpu().numpy() # single clip features extracted
-            v = v / np.sqrt(np.sum(v**2)) # single clip features normalized (L2)
-            full_features[i].append(v)
+            v = features  # single clip features extracted
+            v = v / torch.linalg.norm(v)  # single clip features normalized (L2)
+            full_features[batch_id, i, :, :, :, :, :] = v
 
-    full_features = [np.concatenate(i, axis=0) for i in full_features]
-    full_features = [np.expand_dims(i, axis=0) for i in full_features]
-    full_features = np.concatenate(full_features, axis=0)
-    full_features = full_features[:,:,:,0,0,0]
-    full_features = np.array(full_features).transpose([1,0,2])
+    full_features = full_features.cpu().numpy()
 
-    return full_features
+    # from fixed tensor to variable length list
+    out = [[] for i in range(n_crops)]
+    for batch_id in range(n_batches):
+        for i in range(n_crops):
+            if batch_id == n_batches - 1:
+                # ignore padded values in the last batch
+                out[i].append(full_features[batch_id, i, :k, :, :, :, :])
+            else:
+                out[i].append(full_features[batch_id, i, :, :, :, :, :])
+
+    out = [np.concatenate(i, axis=0) for i in out]
+    out = [np.expand_dims(i, axis=0) for i in out]
+    out = np.concatenate(out, axis=0)
+    out = out[:, :, :, 0, 0, 0]
+    out = np.array(out).transpose([1, 0, 2])
+
+    return out
 
 
-def generate(datasetpath, outputpath, pretrainedpath, frequency, clip_size, batch_size):
-    Path(outputpath).mkdir(parents=True, exist_ok=True)
-    temppath = outputpath+ "/temp/"
-    rootdir = Path(datasetpath)
-    #videos = [str(f) for f in rootdir.glob('**/*.mp4')]
-    videos = [str(f) for f in rootdir.glob('**/*.avi')]
+def generate(dataset_path, output_path, pretrained_path, clip_size, batch_size):
+    Path(output_path).mkdir(parents=True, exist_ok=True)
+    temp_path = output_path + "/temp/"
+    root_dir = Path(dataset_path)
+    # videos = [str(f) for f in root_dir.glob('**/*.mp4')]
+    videos = [str(f) for f in root_dir.glob('**/*.avi')][:3]
 
-    # setup the model
-    i3d = i3_res50(400, pretrainedpath)
-    i3d.cuda()
+    # set up the model
+    torch.backends.cudnn.benchmark = True
+    torch.manual_seed(42)
+    torch.cuda.empty_cache()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    i3d = i3_res50(400, pretrained_path)
+    i3d.to(device)
     i3d.train(False)  # Set model to evaluate mode
 
     for video in videos:
+        frames_path = video.split(dataset_path)[1].split('.')[0]
+        frames_path = os.path.join('/home/ubuntu/Desktop/tmp', frames_path)
+
+        # remove files into temp folder
+        # for f in Path(temp_path).glob('**/*'):
+        #    os.remove(f)
+
         # create temp folder
-        Path(temppath).mkdir(parents=True, exist_ok=True)
+        # Path(temp_path).mkdir(parents=True, exist_ok=True)
 
         # splitting the video into frames
-        ffmpeg.input(video).output('{}%d.jpg'.format(temppath),start_number=0).global_args('-loglevel', 'quiet').run()
+        # ffmpeg.input(video).output('{}%d.jpg'.format(temp_path), start_number=0).global_args('-loglevel', 'quiet').run()
 
         # extract features
-        features = extract_features(i3d, clip_size, frequency, temppath, batch_size)
+        features = extract_features(i3d, device, clip_size, frames_path, batch_size)
 
         # save features
-        videoname = video.split("/")[-1].split(".")[0]
-        np.save(outputpath + "/" + videoname, features)
+        video_name = video.split("/")[-1].split(".")[0]
+        np.save(output_path + "/" + video_name, features)
+
+        print('features of dimension {} extracted from {}'.format(features.shape, video))
 
 
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datasetpath', type=str, default="samplevideos/")
-    parser.add_argument('--outputpath', type=str, default="output")
-    parser.add_argument('--pretrainedpath', type=str, default="pretrained/i3d_r50_kinetics.pth")
-    parser.add_argument('--frequency', type=int, default=16)
-    parser.add_argument('--clip_size', type=int, default=16)
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--dataset_path', type=str, default="ShanghaiTech_new_split/test/")
+    parser.add_argument('--output_path', type=str, default="output")
+    parser.add_argument('--pretrained_path', type=str, default="pretrained/i3d_r50_kinetics.pth")
+    parser.add_argument('--clip_length', type=int, default=16)
+    parser.add_argument('--batch_size', type=int, default=32)
     args = parser.parse_args()
-    generate(args.datasetpath, str(args.outputpath), args.pretrainedpath, args.frequency, args.clip_size, args.batch_size)
+    generate(args.dataset_path, str(args.output_path), args.pretrained_path, args.clip_length, args.batch_size)
+
+# python3 main.py --dataset_path=UCF-Crime-Test/ --output_path=output
+# python3 main.py --dataset_path=ShanghaiTech_new_split/test/ --output_path=output
--- utils/utils.py
+++ utils/utils.py
@@ -1,65 +1,91 @@
 import numpy as np
 import torch
-import torchvision.transforms as transforms
 import os
 import cv2
-
+from opencv_transforms import transforms as T
+from concurrent.futures import ThreadPoolExecutor
 
 
 def from_frames_to_clips(frames, step=16):
     frame_cnt = len(frames)
     stack_depth = 16
 
-    frame_indices = [] # Frames to chunks
+    frame_indices = []  # Frames to chunks
     frame_ticks = range(1, frame_cnt + 1, step)
-    for tick in frame_ticks: # for clip in frames
+    for tick in frame_ticks:  # for clip in frames
         # frame indices for a clip
         frame_idx = [min(frame_cnt, tick + offset) for offset in range(stack_depth)]
         frame_indices.append(frame_idx)
-    return np.asarray(frame_indices)
+    return np.asarray(frame_indices, dtype=object) - 1  # indices start from 0
+
+
+def divide_chunks(l, n):
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 
 def split_clip_indices_into_batches(frame_indices, batch_size):
-    chunk_num = frame_indices.shape[0]
-    batch_num = int(np.ceil(chunk_num / batch_size)) # Chunks to batches
-    frame_indices = np.array_split(frame_indices, batch_num, axis=0) # batch_size x (dimensione di clip che fittano in una batch)
-    return np.asarray(frame_indices) - 1
+    frame_indices = list(divide_chunks(list(frame_indices), batch_size))
+
+    y = len(frame_indices[-1])  # number of clips inside the last batch
+    pad = batch_size - y  # how many clip to fill
+
+    # pad the last batch with a number of (x - y) clips
+    # by repeating the last clip inside the last batch
+    last_batch = [clip for clip in frame_indices[-1]] + [frame_indices[-1][-1]] * pad
+    frame_indices[-1] = last_batch
+
+    # print("Exact size:", (len(frame_indices) - 1) * batch_size + y)
+
+    return np.asarray(frame_indices, dtype=np.uint32), y
 
 
 # Normalize the video frames with mean and standard deviation calculated across all images
-# ResNet50 was pretrained on ImageNet Than trained on Kinetics
-def kinetics_mean_std():
+# ResNet50 was pretrained on ImageNet Than trained on Kinetics same for the C3D model trained on the Sports-1M
+def imagenet_mean_std():
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
     return mean, std
 
 
-def load_rgb_batch(frames_dir, rgb_files, frame_indices):
-    batch_data = np.zeros(frame_indices.shape + (10, 3, 224, 224))  # B=16 x T=16 x CROP=10 x CH=3 x H=224 x W=224
-
-    mean, std = kinetics_mean_std()
-    crop_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean, std),
+def transform_frame(frame_path):
+    mean, std = imagenet_mean_std()
+    crop_transform = T.Compose([
+        T.ToTensor(),
+        T.Normalize(mean, std),
     ])
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize(240), # for the UCF-CRIME is 240 else 256
-        transforms.TenCrop(224),
-        transforms.Lambda(lambda crops: torch.stack([crop_transform(crop) for crop in crops])),
+    transform = T.Compose([
+        T.Resize(240),  # for the UCF-CRIME is 240
+        T.TenCrop(224),
+        T.Lambda(lambda crops: torch.stack([crop_transform(crop) for crop in crops])),
     ])
 
-    # frame_indices.shape[0] = num of batches
-    for batch_index in range(frame_indices.shape[0]):
+    # read a single frame
+    frame = cv2.imread(frame_path)
 
-        # frame_indices.shape[1] = num of frames inside a clip
-        for frame_index in range(frame_indices.shape[1]):
-            # read a single frame
-            frame = cv2.imread(os.path.join(frames_dir, rgb_files[frame_indices[batch_index][frame_index]]))
+    # apply data augmentation
+    frame = transform(frame)
+    return frame
 
-            # apply data augmentation
-            frame = transform(frame)
 
-            batch_data[batch_index, frame_index, :, :, :, :] = frame
+def load_rgb_batch(frames_dir, rgb_files, frame_indices, device):
+    # B=16 x T=16 x CROP=10 x CH=3 x H=224 x W=224
+    out = torch.zeros(frame_indices.shape + (10, 3, 224, 224), device=device)
 
-    return batch_data
+    # num of clips
+    n_clips = frame_indices.shape[0]
+    for i in range(n_clips):
+
+        # num of frames inside a clip
+        n_frames = frame_indices.shape[1]
+        frames_path = [os.path.join(frames_dir, rgb_files[frame_indices[i][j]]) for j in range(n_frames)]
+
+        frames = None
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            frames = list(executor.map(transform_frame, frames_path))
+
+        for j in range(len(frames)):
+            out[i, j, :, :, :, :] = frames[j]
+
+    return out
